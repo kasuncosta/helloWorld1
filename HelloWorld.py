cbd47b3a21af13cb1d616f90be17ba0f1042cb93
kasuncosta <kasuncosta@gmail.com>
2021-01-20 19:45:48 -0500

    IntelJ 1st commit

--- HelloWorld.py
+++ HelloWorld.py
@@ -9,6 +9,8 @@ x_list = [x]*10
 y_list = [y]*10
 big_list = x_list + y_list
 
+#try commit using intelJ
+
 print("x_list contains %d objects" % len(x_list))
 print("y_list contains %d objects" % len(y_list))
 print("big_list contains %d objects" % len(big_list))
